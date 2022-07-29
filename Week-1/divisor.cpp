77ad2d3f7df736d2bdfd2d781194c47fb2bd0043
Nikita <pga.nikita@gmail.com>
2022-07-29 20:22:39 +0530

    Week-1

--- Week-1/divisor.cpp
+++ Week-1/divisor.cpp
@@ -1,38 +1,31 @@
 /***
+Problem statement:
 You are given integers N and M.
+Consider a sequence of length N consisting of positive integers such that a1​+a2​+...+aN​ = M. 
+Find the maximum possible value of the greatest common divisor of a1​,a2​,...,aN​.
 
-Consider a sequence 
-a of length N consisting of positive integers such that a1​+a2​+...+aN​ = M. Find the maximum possible value of the greatest common divisor of 
-
-a1​,a2​,...,aN​.
-Constraints
-
+Constraints:
     All values in input are integers.
+	1≤N≤10^5
+    N≤M≤10^9
 
-1≤N≤105
-
-    N≤M≤109
-
-Input
-
+Input:
 Input is given from standard input in the following format:
-
 N M
 
-Output
-
+Output:
 Print the maximum possible value of the greatest common divisor of a sequence a1​,a2​,...,aN​ that satisfies the condition.
 
-Examples
-Sample Input 1
+Examples:
+	Sample Input 1
+	3 14
 
-3 14
+	Sample Output 1
+	2
 
-Sample Output 1
-
-2
-
-Consider the sequence (a1​,a2​,a3​)=(2,4,8). Their greatest common divisor is 2, and this is the maximum value. In particular, there is no sequence involving numbers larger than two for which the sum is 14 and the GCD is larger than two.
+	Explanation
+	Consider the sequence (a1​,a2​,a3​)=(2,4,8). Their greatest common divisor is 2, and this is the maximum value. 
+	In particular, there is no sequence involving numbers larger than two for which the sum is 14 and the GCD is larger than two.
 ****/
 
 
@@ -42,6 +35,7 @@ using namespace std;
 vector<int> getDivisors(int M) {
     vector<int> divisors;
     
+	/* Find the divisors of M and store in vector*/
     for(int i=1;i<M;i++) {
         if (M%i == 0)
             divisors.push_back(i);
@@ -54,6 +48,7 @@ int main() {
 	cin >> N >> M;
 	vector<int> divisors = getDivisors(M);
 	int ans = 1;
+	/* Find the largest divisor D which satisfies the condition M >= N*D */
 	for(int i=0; i < divisors.size(); i++) {
 	    if (N <= M / divisors[i]) {
 	        ans = divisors[i];
